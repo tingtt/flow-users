02f8a02cac3aee9041cc9b6ba134460500dcaae7
tingtt <ting.taku@gmail.com>
2022-02-24 19:45:30 +0900

    [update] mysqlのパスワードのログ非表示

--- mysql/mysql.go
+++ mysql/mysql.go
@@ -12,7 +12,7 @@ var dsn string
 
 func SetDSNTCP(user string, password string, host string, port int, db string) string {
 	dsn = fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", user, password, host, port, db)
-	return dsn
+	return fmt.Sprintf("%s:********@tcp(%s:%d)/%s", user, host, port, db)
 }
 
 func Open() (*sql.DB, error) {
