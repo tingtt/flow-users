aaf7bee79202af8eec6c61783e40ffbd5e2bdbc1
tingtt <ting.taku@gmail.com>
2022-02-24 10:31:25 +0900

    [fix] バリデーションエラーのレスポンス

--- main.go
+++ main.go
@@ -63,7 +63,7 @@ type CustomValidator struct {
 func (cv *CustomValidator) Validate(i interface{}) error {
 	if err := cv.validator.Struct(i); err != nil {
 		// Optionally, you could return the error to give each route more control over the status code
-		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+		return echo.NewHTTPError(http.StatusUnprocessableEntity, err.Error())
 	}
 	return nil
 }
