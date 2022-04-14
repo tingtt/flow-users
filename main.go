32c4b6756191b0a8d4e9e0c22a0d405ba98a5ee5
tingtt <ting.taku@gmail.com>
2022-04-14 14:25:46 +0900

    [add] logger middleware

--- main.go
+++ main.go
@@ -66,10 +66,38 @@ func (cv *CustomValidator) Validate(i interface{}) error {
 	return nil
 }
 
+func logFormat() string {
+	// Refer to https://github.com/tkuchiki/alp
+	var format string
+	format += "time:${time_rfc3339}\t"
+	format += "host:${remote_ip}\t"
+	format += "forwardedfor:${header:x-forwarded-for}\t"
+	format += "req:-\t"
+	format += "status:${status}\t"
+	format += "method:${method}\t"
+	format += "uri:${uri}\t"
+	format += "size:${bytes_out}\t"
+	format += "referer:${referer}\t"
+	format += "ua:${user_agent}\t"
+	format += "reqtime_ns:${latency}\t"
+	format += "cache:-\t"
+	format += "runtime:-\t"
+	format += "apptime:-\t"
+	format += "vhost:${host}\t"
+	format += "reqtime_human:${latency_human}\t"
+	format += "x-request-id:${id}\t"
+	format += "host:${host}\n"
+	return format
+}
+
 func main() {
 	e := echo.New()
 	e.Use(middleware.GzipWithConfig(middleware.GzipConfig{
 		Level: *gzipLevel,
+	}))
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: logFormat(),
+		Output: os.Stdout,
 	}))
 	e.Logger.SetLevel(log.Lvl(*logLevel))
 	e.Validator = &CustomValidator{validator: validator.New()}
