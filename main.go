bf240fe1c2d60b0c681b4def128ef7b1ac2678b0
tingtt <ting.taku@gmail.com>
2022-07-13 13:44:58 +0900

    [add] logging

--- main.go
+++ main.go
@@ -64,10 +64,15 @@ func main() {
 	// Echo instance
 	e := echo.New()
 
+	// Log level
+	e.Logger.SetLevel(log.Lvl(*f.LogLevel))
+	e.Logger.Infof("Log level %d", *f.LogLevel)
+
 	// Gzip
 	e.Use(middleware.GzipWithConfig(middleware.GzipConfig{
 		Level: int(*f.GzipLevel),
 	}))
+	e.Logger.Infof("Gzip enabled with level %d", *f.GzipLevel)
 
 	// CORS
 	if f.AllowOrigins != nil && len(f.AllowOrigins) != 0 {
@@ -75,22 +80,9 @@ func main() {
 			AllowOrigins: f.AllowOrigins,
 			AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
 		}))
+		e.Logger.Info("CORS enabled")
+		e.Logger.Debugf("CORS allow origins %v", f.AllowOrigins)
 	}
-
-	// Logger
-	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-		Format: logFormat(),
-		Output: os.Stdout,
-		Skipper: func(c echo.Context) bool {
-			return c.Path() == "/-/readiness"
-		},
-	}))
-
-	// Log level
-	e.Logger.SetLevel(log.Lvl(*f.LogLevel))
-
-	// Validator instance
-	e.Validator = &CustomValidator{validator: validator.New()}
 
 	// JWT
 	e.Use(middleware.JWTWithConfig(middleware.JWTConfig{
@@ -104,12 +96,27 @@ func main() {
 		},
 	}))
 
+	// Logger
+	if f.LogLevel != nil && *f.LogLevel == 1 {
+		e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+			Format: logFormat(),
+			Output: os.Stdout,
+			Skipper: func(c echo.Context) bool {
+				return c.Path() == "/-/readiness"
+			},
+		}))
+		e.Logger.Info("Access logging with `alp`(https://github.com/tkuchiki/alp) enabled")
+	}
+
+	// Validator instance
+	e.Validator = &CustomValidator{validator: validator.New()}
+
 	//
 	// Setup DB
 	//
 
 	// DB client instance
-	e.Logger.Info(mysql.SetDSNTCP(*f.MysqlUser, *f.MysqlPasswd, *f.MysqlHost, int(*f.MysqlPort), *f.MysqlDB))
+	e.Logger.Debugf("DB DSN `%s`", mysql.SetDSNTCP(*f.MysqlUser, *f.MysqlPasswd, *f.MysqlHost, int(*f.MysqlPort), *f.MysqlDB))
 
 	// Check connection
 	d, err := mysql.Open()
@@ -119,22 +126,32 @@ func main() {
 	if err = d.Ping(); err != nil {
 		e.Logger.Fatal(err)
 	}
+	e.Logger.Info("DB connection test succeeded")
 
 	//
 	// Setup OAuth2 providers
 	//
 
 	// Github
-	if _, err := github.New(*f.GithubClientId, *f.GithubClientSecret); err != nil {
-		e.Logger.Error(err.Error())
+	if f.GithubClientId != nil && *f.GithubClientId != "" && f.GithubClientSecret != nil && *f.GithubClientSecret != "" {
+		if _, err := github.New(*f.GithubClientId, *f.GithubClientSecret); err != nil {
+			e.Logger.Error(err.Error())
+		}
+		e.Logger.Info("OAuth2 provider GitHub availabled")
 	}
 	// Google
-	if _, err := google.New(*f.GoogleClientId, *f.GoogleClientSecret); err != nil {
-		e.Logger.Error(err.Error())
+	if f.GoogleClientId != nil && *f.GoogleClientId != "" && f.GoogleClientSecret != nil && *f.GoogleClientSecret != "" {
+		if _, err := google.New(*f.GoogleClientId, *f.GoogleClientSecret); err != nil {
+			e.Logger.Error(err.Error())
+		}
+		e.Logger.Info("OAuth2 provider Google availabled")
 	}
 	// Twitter
-	if _, err := twitter.New(*f.TwitterClientId, *f.TwitterClientSecret); err != nil {
-		e.Logger.Error(err.Error())
+	if f.TwitterClientId != nil && *f.TwitterClientId != "" && f.TwitterClientSecret != nil && *f.TwitterClientSecret != "" {
+		if _, err := twitter.New(*f.TwitterClientId, *f.TwitterClientSecret); err != nil {
+			e.Logger.Error(err.Error())
+		}
+		e.Logger.Info("OAuth2 provider Twitter availabled")
 	}
 
 	//
